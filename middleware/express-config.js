13c16eb7e91a95dcc0021fb5c61ed6b3309e69d2
Desmond dai <desmonddai583@gmail.com>
2014-10-29 17:59:43 +0800

    add store session in mongo

--- middleware/express-config.js
+++ middleware/express-config.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var session = require('express-session');
+var MongoStore = require('connect-mongostore')(session);
 var cookieParser = require('cookie-parser');
 var passport = require('passport');
 var jade = require('jade');
@@ -14,9 +15,10 @@ module.exports = function(app) {
   app.use(bodyParser.json());
   app.use(cookieParser());
   app.use(session({ secret: 'desmond.dai',
-                    cookie: { maxAge: 60000 },
+                    cookie: { maxAge: 100000 },
                     saveUninitialized: true,
-                    resave: true
+                    resave: true,
+                    store: new MongoStore({'db': 'telegram-sessions'})
                   }));
   app.use(passport.initialize());
   app.use(passport.session());
