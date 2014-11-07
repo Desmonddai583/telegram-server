32a92ba1f4cdced15bde06d200f8d35a5a555ea6
Desmond dai <desmonddai583@gmail.com>
2014-11-08 00:30:53 +0800

    add more session time

--- send-digest.js
+++ send-digest.js
@@ -2,7 +2,7 @@ var logger = require('nlogger').logger(module);
 var SQS = require('aws-sqs');
 var sqs = new SQS('AKIAI6D7YGM45PYLHS6A', '42zLfXr/jErpyM4+lZFMriU+aBUPjkUyqpGMDwSc');
 
-sqs.sendMessage('/410293358835/sendDigest', 'test message', function(err, res) {
+sqs.sendMessage('/410293358835/sendDigest', {}, function(err, res) {
   if(err) {
     logger.error(err);
   }
