5564892dc910b52dd02862fe5ca6963a60e38f92
Desmond dai <desmonddai583@gmail.com>
2014-11-10 12:44:11 +0800

    add cron job

--- send-digest.js
+++ send-digest.js
@@ -1,10 +1,21 @@
 var logger = require('nlogger').logger(module);
+var CronJob = require('cron').CronJob;
 var SQS = require('aws-sqs');
 var sqs = new SQS('AKIAI6D7YGM45PYLHS6A', '42zLfXr/jErpyM4+lZFMriU+aBUPjkUyqpGMDwSc');
 
-sqs.sendMessage('/410293358835/sendDigest', {}, function(err, res) {
-  if(err) {
-    logger.error(err);
-  }
-  logger.info(res);
-});
+var job = new CronJob('0 0 8 * * *', function(){
+    sqs.sendMessage('/410293358835/sendDigest', 'hello', function(err, res) {
+      if(err) {
+        logger.error(err);
+      }
+      logger.info(res);
+    });
+  }, function () {
+    logger.info('Cron job execution is completed!');
+  },
+  true
+);
+
+
+
+
