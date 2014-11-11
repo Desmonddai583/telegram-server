72f400390fe3c498cc0764962b944a1b1710df38
Desmond dai <desmonddai583@gmail.com>
2014-11-11 17:32:51 +0800

    sqs WIP

--- workers/digest-worker.js
+++ workers/digest-worker.js
@@ -2,13 +2,13 @@ var logger = require('nlogger').logger(module);
 var Consumer = require('sqs-consumer');
 var async = require("async");
 var mongoose = require('mongoose');
-var nconf = require('./middleware/nconf-config');
-var db = require('./database/database');
+var nconf = require('../middleware/nconf-config');
+var db = require('../database/database');
 var AWS = require('aws-sdk');
 var sqs = new AWS.SQS({accessKeyId: nconf.get('sqs:aws-id'), secretAccessKey: nconf.get('sqs:aws-secret'), region: nconf.get('sqs:region')});
 var User = mongoose.model('User');
 var Post = mongoose.model('Post');
-var mailer = require('./utils/mailer');
+var mailer = require('../utils/mailer');
 
 db.once('open', function callback () {
   var app = new Consumer({
