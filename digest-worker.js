fab5c0f6e11036b3a28ffdbbce35f205f4f2f29a
Desmond dai <desmonddai583@gmail.com>
2014-11-11 15:52:50 +0800

    sqs WIP

--- digest-worker.js
+++ digest-worker.js
@@ -2,15 +2,15 @@ var logger = require('nlogger').logger(module);
 var mongoose = require('mongoose');
 var nconf = require('./middleware/nconf-config');
 var AWS = require('aws-sdk');
-var sqs = new AWS.SQS({accessKeyId: nconf.get('sqs:aws-id'), secretAccessKey: nconf.get('sqs:aws-secret'), region: "us-east-1"});
+var sqs = new AWS.SQS({accessKeyId: nconf.get('sqs:aws-id'), secretAccessKey: nconf.get('sqs:aws-secret'), region: nconf.get('sqs:region')});
 var Consumer = require('sqs-consumer');
 var db = require('./database/database');
 var User = mongoose.model('User');
 
 db.once('open', function callback () {
   var app = new Consumer({
-    queueUrl: 'https://sqs.us-east-1.amazonaws.com/410293358835/sendDigest',
-    region: 'us-east-1',
+    queueUrl: nconf.get('sqs:queue-url'),
+    region: nconf.get('sqs:region'),
     sqs: sqs,
     handleMessage: function (message, done) {
       logger.info(message)
